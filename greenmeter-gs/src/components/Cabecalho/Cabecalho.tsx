9539465962f04afa426db712048211a7dd2fb154
victorcosmedev <victorcosme.profissional@gmail.com>
2024-11-16 18:05:37 -0300

    Hero e cabeçalho formatado em todos os breakpoints

--- greenmeter-gs/src/components/Cabecalho/Cabecalho.tsx
+++ greenmeter-gs/src/components/Cabecalho/Cabecalho.tsx
@@ -3,19 +3,25 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <header className="bg-gradient-to-b from-black/50 from-80% text-white text-sm h-[5vh] flex justify-between items-center px-[6%]">
+    <header className="flex justify-between items-center bg-gradient-to-b from-black/50 from-80% text-white text-sm h-[5vh] px-[6%] pb-1 sm:h-[7vh] sm:[pt-3] sm:pb-2  sm:text-base md:text-lg lg:h-[9vh]">
       <h1 className="font-semibold">
         <span className="text-limao">Green</span>Meter
       </h1>
-      <Link href="" className="h-[60%] w-auto">
+      <Link href="" className="h-[50%] w-auto">
         <Image
           src="/img/icone-menu.png"
           alt="ícone de menu"
           width={200}
           height={100}
-          className="h-full w-auto"
+          className="h-full w-auto md:hidden"
         />
       </Link>
+      <div className="hidden md:flex gap-3">
+        <Link href="/sobre-nos" className="font-semibold">Sobre nós</Link>
+        <Link href="/sobre" className="font-semibold">Sobre nós</Link>
+        <Link href="/sobre" className="font-semibold">Sobre nós</Link>
+
+      </div>
     </header>
   );
 }
--- greenmeter-gs/src/components/Hero/Hero.tsx
+++ greenmeter-gs/src/components/Hero/Hero.tsx
@@ -2,12 +2,12 @@ import Cabecalho from "../Cabecalho/Cabecalho";
 
 export default function Hero() {
   return (
-    <div className="bg-hero bg-cover h-[27vh]">
+    <div className="bg-hero bg-cover h-[27vh] sm:h-[35vh] lg:h-[40vh]">
       <div className="h-full w-full bg-black/50">
         <Cabecalho/>
-        <div className="flex flex-col gap-3 justify-center font-semibold text-white h-[22vh] px-4 text-xs">
+        <div className="flex flex-col gap-3 justify-center font-semibold text-white  px-4 text-xs sm:text-base sm:gap-7 sm:px-7 md:text-lg md:px-[6%] h-[22vh] sm:h-[28vh] lg:h-[31vh] ">
           <div className="w-[60%]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
-          <div className="rounded text-musgo text-center px-1 py-1 bg-slate-200 w-[30%] ">
+          <div className="rounded text-musgo text-center px-3 py-1 bg-slate-200 w-fit">
             Saiba mais
           </div> 
         </div>
