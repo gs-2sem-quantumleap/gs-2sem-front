019f9afb0a01854e561612e12fda94db3f778b25
arthur-pulini <arthurpulini@gmail.com>
2024-11-21 16:30:56 -0300

    Estilizacao da pagina not found

--- greenmeter-gs/src/app/not-found.tsx
+++ greenmeter-gs/src/app/not-found.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404: Page Not Found</h1>
-    </div>
+    <main className="md:container mx-auto px-[5%] py-[3vh] text-sm md:text-base h-[70vh] flex flex-col items-center justify-center">
+      <h1 className="font-bold text-base">
+        Página<span className="text-musgo"> não encontrada.</span>
+      </h1>
+      <p className="text-center">
+        Acho que você se perdeu... Por favor, volte à{" "}
+        <Link href="/" className="underline text-blue-600">página principal.</Link>
+      </p>
+    </main>
   );
 }
