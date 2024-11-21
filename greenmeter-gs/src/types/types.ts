947742eca7b272239de974eba1150d6ee4879e2c
arthur-pulini <arthurpulini@gmail.com>
2024-11-21 16:32:22 -0300

    Criacao de novos tipos conforme necessidade

--- greenmeter-gs/src/types/types.ts
+++ greenmeter-gs/src/types/types.ts
@@ -12,9 +12,31 @@ export type TipoConsumoMorador = {
 }
 
 export type TipoConta = {
-    valorConta: number;
-    dataConta: Date;
     consumoKwh: number;
+    dataConta: DataConta;
     idApartamento: number;
+    idContaDeEnergia: number;
+    valorConta: number;
+}
+
+export type TipoContaInput = {
+    consumoKwh: number;
+    dataConta: Date;
+    idApartamento: number;
+    valorConta: number;
+}
+
+export type TipoContaResponse = {
+    consumoKwh: number;
+    dataConta: [number, number, number];
+    idApartamento: number;
+    idContaDeEnergia: number;
+    valorConta: number;
 }
   
+
+export type DataConta = {
+    dia: number;
+    mes: number;
+    ano: number;
+}
